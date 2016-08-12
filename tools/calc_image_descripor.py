8f7c621654d03ad0cbd606c88251f3275207fc83
Vasilii Chernov <kapot65@gmail.com>
2016-08-12 14:58:41 +0300

    Add code for saving descriptors to csv file

--- tools/calc_image_descripor.py
+++ tools/calc_image_descripor.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import csv
 from PIL import Image
 from argparse import ArgumentParser
 
@@ -13,20 +14,28 @@ from ml.descriptors import image_descriptors
 
 
 def parse_args():
-	parser = ArgumentParser('Tool for calculate image descriptor')
+    parser = ArgumentParser('Tool for calculate image descriptor')
 
-	parser.add_argument('image',   type=args_types.existed_file,       help='Image')
-	parser.add_argument('--descr', choices=image_descriptors.keys(),  help='Descriptor (default: LBP)')
+    parser.add_argument('image',   type=args_types.existed_file,       help='Image')
+    parser.add_argument('--descr', choices=image_descriptors.keys(),  help='Descriptor (default: LBP)')
+    parser.add_argument('--csv_path', type=str, help='Output csv path')
 
-	parser.set_defaults(descr='LBP')
+    parser.set_defaults(descr='LBP')
 
-	return parser.parse_args()
+    return parser.parse_args()
 
 if __name__  == '__main__':
-	args = parse_args()
+    args = parse_args()
 
-	img = Image.open(args.image)
-	descr = image_descriptors[args.descr](img)
-
-	print(descr)
+    img = Image.open(args.image)
+    descr = image_descriptors[args.descr](img)
+ 
+    if args.csv_path:
+        with open(args.csv_path, "a") as csv_file:
+            writer = csv.writer(csv_file)
+            data = [args.image]
+            data += list(descr[0])
+            writer.writerow(data)
+    else: 
+        print(descr)
 
