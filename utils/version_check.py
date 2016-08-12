786dfb35b8bf73d4c546701a37783b23ee549840
Ivan Nizhnichenko <ivan_nizh@mail.ru>
2016-08-12 18:26:11 +0300

    change python <3 behavior

--- utils/version_check.py
+++ utils/version_check.py
@@ -4,7 +4,6 @@ import logging
 
 if sys.version_info < (3, 0) :
     logging.error("Sorry, requires Python 3.5")
-    exit(1)
 
 if sys.version_info < (3, 5) :
     warnings.warn("We recomend you Python 3.5")
