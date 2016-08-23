7c62ed8f1d2c5175bfa3c2ddd83a51fd807eab5d
yegorf1 <yegorf1@gmail.com>
2016-08-23 13:49:29 +0300

    Speeduped iterate_styles()

--- tags_generator/style_utils.py
+++ tags_generator/style_utils.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 def iterate_styles(styles: list):
 	'''
 		Generates all values fot speciefed array of styles
@@ -5,10 +7,10 @@ def iterate_styles(styles: list):
 		@styles <list> - list of web styles
 	'''
 
-	if len(styles) == 0:
-		yield [ ]
-	else:
-		tag = styles[0]
-		for val in tag.get_values():
-			for othet_styles in iterate_styles(styles[1:]):
-				yield [ [ tag.name, val ] ] + othet_styles
+	# Generates list for product
+
+	pr = [ tag.get_values() for tag in styles ]
+	l = len(pr)
+
+	for p in product(*pr):
+		yield [ [ styles[i].name, p[i] ] for i in range(l) ]
